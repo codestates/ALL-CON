efb31bfab5ea2541fed3c43f44d6ffd6ff841e34
Esoolgnah <nezcoreen@gmail.com>
2022-01-08 10:48:31 +0900

    In Progress: Make Conchin CertificationPage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,23 @@ import React from 'react';
 import ConChinPage from './pages/ConChinPage';
 import MainPage from './pages/MainPage';
 import ConcertPage from './pages/ConcertPage';
+import ConChinCertificationPage from './pages/ConChinCertificationPage';
+import MyPage from './pages/MyPage';
 import Header from './components/Header';
 import ConcertModal from './components/Modals/ConcertPage/ConcertModal';
+import MyProfileImageModal from './components/Modals/MyPage/MyProfileImageModal';
 function App() {
   return (
     <div className='App'>
-      <ConcertPage />
+      <Header />
+      {/* <ConcertPage /> */}
+      {/* <MainPage /> */}
+      {/* <ConChinPage /> */}
+      {/* <ConChinCertificationPage /> */}
+      <MyPage />
+      <div id='modalWrapper'>
+        <MyProfileImageModal />
+      </div>
     </div>
   );
 }
