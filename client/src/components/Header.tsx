ed73f550fe445866323473ff5d03c1b759aa7b02
Esoolgnah <nezcoreen@gmail.com>
2022-01-12 15:30:50 +0900

    Done: Make header timer

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,5 +1,5 @@
 /* Config import */
-import { REACT_APP_API_URL } from '../config.js'
+import { REACT_APP_API_URL } from '../config.js';
 /* CSS import */
 import logo from '../images/allConLogo.png';
 import menu from '../images/menu.png';
@@ -20,7 +20,7 @@ function Header() {
   const navigate = useNavigate();
   const { isLogin } = useSelector((state: RootState) => state.auth);
   const { userInfo } = useSelector((state: RootState) => state.auth);
-  const [ scrollPosition, setScrollPosition ] = useState(0);
+  const [scrollPosition, setScrollPosition] = useState(0);
 
   useEffect(() => {
     window.addEventListener('scroll', updateScroll);
@@ -36,21 +36,24 @@ function Header() {
       await axios.post(
         `${REACT_APP_API_URL}/logout`,
         {},
-        { withCredentials: true }
+        { withCredentials: true },
       );
-      
+
       /* 로그인 상태 변경 & main 페이지로 이동 */
       dispatch(logout());
       navigate('/main');
-    } catch(err) {
+    } catch (err) {
       console.log(err);
     }
-  }
+  };
 
   return (
     <div id='headerContainer'>
+      <div id='hiddenBar'>다음 콘서트를 업데이트하기까지 13:49:06</div>
       <div id='logoBar'>
-      <Link to ='/main'><img className='logo' alt='logoImg' src={logo} /></Link>
+        <Link to='/main'>
+          <img className='logo' alt='logoImg' src={logo} />
+        </Link>
       </div>
       <div id={scrollPosition < 48 ? 'absoluteBar' : 'fixedBar'}>
         <div id='menuWrapper'>
@@ -61,13 +64,29 @@ function Header() {
         </div>
         <div id='loginWrapper'>
           {/* 로그인 여부에 따라 프로필 이미지 혹은 로그인 버튼 출력 */}
-          {isLogin ? <img className='profile' alt='profileImg' src={userInfo.image ? userInfo.image : user} onClick={() => logoutHandler()} />
-          : <p className='login' onClick={() => dispatch(showLoginModal(true))}>로그인</p>}
+          {isLogin ? (
+            <img
+              className='profile'
+              alt='profileImg'
+              src={userInfo.image ? userInfo.image : user}
+              onClick={() => logoutHandler()}
+            />
+          ) : (
+            <p className='login' onClick={() => dispatch(showLoginModal(true))}>
+              로그인
+            </p>
+          )}
         </div>
         <div id='hiddenMenuBox'>
-          <Link to ='/main'><p className='menu'>홈</p></Link>
-          <Link to ='/concert'><p className='menu'>콘서트</p></Link>
-          <Link to ='/conchin'><p className='menu'>콘친 찾기</p></Link>
+          <Link to='/main'>
+            <p className='menu'>홈</p>
+          </Link>
+          <Link to='/concert'>
+            <p className='menu'>콘서트</p>
+          </Link>
+          <Link to='/conchin'>
+            <p className='menu'>콘친 찾기</p>
+          </Link>
         </div>
         <div id='hiddenSearchBox'>
           <div id='searchWrapper'>
