994713541b1094e4ced3d73f301226a5bf3b9df0
sun0-1106 <sy.choi1106@gmail.com>
2022-01-13 09:39:32 +0900

    In Progress:Make Jumbotron Works

--- client/src/components/PosterSlide.tsx
+++ client/src/components/PosterSlide.tsx
@@ -1,13 +1,25 @@
+/* CSS import */
 import crown from '../images/crown.png';
 import sf9 from '../images/sf99.jpg';
 import six from '../images/six.gif';
 import victon from '../images/victon.gif';
 import hiphop2 from '../images/hiphop2.gif';
 import kimjh from '../images/kimjh.jpg';
-import { useState } from 'react';
+import defaultPoster from '../images/default_poster.jpg';
+/* Store import */
+import { RootState } from '../index';
+import { setOrder, setTarget } from '../store/MainSlice';
+/* Library import */
+import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 function PosterSlide() {
-  const [close, setClose] = useState(true);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const { target } = useSelector((state: RootState) => state.main);
 
   return (
     <>
@@ -16,27 +28,37 @@ function PosterSlide() {
           <img id='posterCrown' src={crown} alt='왕관아이콘'></img>
         </div>
         <div id='posterWrapper1'>
-          <img alt='포스터' src={six} className='posterImg' id='poster'></img>
-          <div className='dDay'>
-            <p>D-5</p>
-          </div>
-        </div>
-        <div id='posterWrapper2'>
-          <img alt='포스터' src={sf9} className='posterImg' id='poster'></img>
-          <div className='posterCover'></div>
-        </div>
-        <div id='posterWrapper3'>
-          <img alt='포스터' src={kimjh} className='posterImg' id='poster'></img>
-          <div className='posterCover'></div>
-        </div>
-        <div id='posterWrapper4'>
           <img
             alt='포스터'
-            src={hiphop2}
+            src={defaultPoster}
             className='posterImg'
             id='poster'
           ></img>
           <div className='posterCover2'></div>
+        </div>
+        <div id='posterWrapper2'>
+          <img
+            alt='포스터'
+            src={defaultPoster}
+            className='posterImg'
+            id='poster'
+          ></img>
+          <div className='posterCover'></div>
+        </div>
+        <div id='posterWrapper3'>
+          <img
+            alt='포스터'
+            src={target.image_concert}
+            className='posterImg'
+            id='poster'
+          ></img>
+          <div className='dDay'>
+            <p>D-5</p>
+          </div>
+        </div>
+        <div id='posterWrapper4'>
+          <img alt='포스터' src={six} className='posterImg' id='poster'></img>
+          <div className='posterCover'></div>
         </div>
         <div id='posterWrapper5'>
           <img
