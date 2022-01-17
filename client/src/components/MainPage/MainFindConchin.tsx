0b6ba5e553fcd6199542f8f5ec6a387535326aa7
JH8459 <kk_ong2233@naver.com>
2022-01-17 20:24:14 +0900

    In Progress: main

--- client/src/components/MainPage/MainFindConchin.tsx
+++ client/src/components/MainPage/MainFindConchin.tsx
@@ -1,13 +1,18 @@
+/* CSS import */
 import goConchin from '../../images/goConChin.png';
+/* Library import */
+import { useNavigate } from 'react-router-dom';
 
 function MainFindConchin() {
+  const navigate = useNavigate();
+
   return (
     <div id='mainGoConchin'>
       <b>
         콘서트에 같이 갈 "<u>콘친</u>" 을 찾아볼까요?
       </b>
       <img src={goConchin} id='goConchin' alt='콘친찾기 일러스트'></img>
-      <button>바로가기</button>
+      <button onClick={() => navigate('/conchin')}>바로가기</button>
     </div>
   );
 }
