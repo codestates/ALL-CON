8eb4465b032b1c98eef6ec4f7c688cac67158bf3
sun0-1106 <sy.choi1106@gmail.com>
2022-01-22 11:01:52 +0900

    Done : Make Jumbotron works,In Progress: Edit Jumbotron css

--- server/controllers/concert/all.js
+++ server/controllers/concert/all.js
@@ -6,15 +6,20 @@ module.exports = {
     try {
       // 로그인 인증 검사
       const userInfo = await userAuth(req, res);
-      
-      // 나의 모든 알람 리스트
-      const myAllAlarmInfo = await Alarms.findAll({
-        where: { user_id: userInfo.dataValues.id },
-      });
-      return res.status(200).json({
-        data: { myAllAlarmInfo: myAllAlarmInfo },
-        message: 'Success Get My All Alarm List!',
-      });
+      console.log('====', userInfo.dataValues);
+
+      /* 에러 임시코드 */
+      if(userInfo.dataValues){
+        const myAllAlarmInfo = await Alarms.findAll({
+          where: { user_id: userInfo.dataValues.id },
+        });
+        return res.status(200).json({
+          data: { myAllAlarmInfo: myAllAlarmInfo },
+          message: 'Success Get My All Alarm List!',
+        });
+      } else {
+        console.log("======버그(임시코드)=====");
+      }
     } catch (err) {
       console.log(err);
       return res.status(500).json({ message: 'Server Error!' });
