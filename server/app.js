33558618c1bd6e4535afeeececd0729f1b51da65
Esoolgnah <nezcoreen@gmail.com>
2022-01-23 16:54:14 +0900

    In Progress: add conchinpage & header animation

--- server/app.js
+++ server/app.js
@@ -13,10 +13,10 @@ const app = express();
 const port = 8080;
 
 // 콘서트 티켓 오픈일 알라머 실행
-const autoAlarm = schedule.scheduleJob('00 24 * * * *', async () => {
-  concertAlarm();
-  console.log('24시간마다 콘서트 알람중..');
-});
+// const autoAlarm = schedule.scheduleJob('00 24 * * * *', async () => {
+//   concertAlarm();
+//   console.log('24시간마다 콘서트 알람중..');
+// });
 
 // 콘서트 클리너 실행
 // const autoConcertCleaner = schedule.scheduleJob(
